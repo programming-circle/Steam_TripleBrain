e88ce4eb70a226fc2961a7099f265eeb683cb4d9
KiraYoshikage1999 <alexblackcat72@gmail.com>
2026-03-25 17:31:47 +0300

    Add project files.

---
+++ Steam_TripleBrain/Data/DataBaseContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Steam_TripleBrain.Data
+{
+    public class DataBaseContext : DbContext //Possibly if needed you can add an IdentityDbContext for user management
+    {
+        public DataBaseContext(DbContextOptions<DataBaseContext> options) : base(options)
+        {
+        }
+        
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            // Configure your entities and relationships here
+        }
+    }
+}
