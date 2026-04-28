bc31546f3d771d84e6135b8366e547734f78a9b9
KiraYoshikage1999 <alexblackcat72@gmail.com>
2026-04-28 18:34:56 +0300

    Fix commit of order

--- Steam_TripleBrain/CQRS/Query/GetProfileQuery.cs
+++
@@ -1,7 +0,0 @@
-﻿using MediatR;
-using Steam_TripleBrain.Models;
-
-namespace Steam_TripleBrain.CQRS.Query
-{
-    public record GetProfileQuery(string Username) : IRequest<Profiles>; // Запит для отримання профілю за ім'ям користувача, повертає об'єкт Profiles
-}
--- Steam_TripleBrain/CQRS/Query/GetTokenLogsQuery.cs
+++
@@ -1,7 +0,0 @@
-﻿using MediatR;
-using Steam_TripleBrain.Models;
-
-namespace Steam_TripleBrain.CQRS.Query
-{
-    public record GetTokenLogsQuery(string Username) : IRequest<IEnumerable<TokenLogs>>; // Запит для отримання логів токенів за ім'ям користувача, повертає колекцію об'єктів TokenLogs
-}
